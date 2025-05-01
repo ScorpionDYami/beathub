da556a0ac1841ee4fe6d244bf29cecba6e0bab38
lucas <lucas@lucas-Latitude-E6440>
2025-05-01 15:54:04 -0700

    Paquete Clases

---
+++ src/main/java/mx/itson/beathub/utils/entities/Horario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.beathub.utils.entities;
+
+/**
+ *
+ * @author lucas
+ */
+public class Horario {
+    
+}
---
+++ src/main/java/mx/itson/beathub/utils/entities/Paciente.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.beathub.utils.entities;
+
+/**
+ *
+ * @author lucas
+ */
+public class Paciente {
+    
+    private String nombre = new String();
+    private String alergias = new String();
+    private String edad = new String();
+    private String tipoSangre = new String();
+    private int masaCorporal = 0;
+    private String expediente = new String();
+    private String phone = new String();
+    private String email = new String();
+    private boolean estado = false ;
+    
+    public Paciente() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
+
+    public void setTipoSangre(String tipoSangre) {
+        this.tipoSangre = tipoSangre;
+    }
+
+    public int getMasaCorporal() {
+        return masaCorporal;
+    }
+
+    public void setMasaCorporal(int masaCorporal) {
+        this.masaCorporal = masaCorporal;
+    }
+
+    public String getExpediente() {
+        return expediente;
+    }
+
+    public void setExpediente(String expediente) {
+        this.expediente = expediente;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
